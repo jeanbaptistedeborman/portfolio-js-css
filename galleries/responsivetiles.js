8696175816bf84d5750f1c0209dc7ffe2ca4a5a9
jeanbaptistedeborman <jb@noloading.com>
2013-10-02 17:47:11 +0200

    Made JSLint compliant

--- galleries/responsivetiles.js
+++ galleries/responsivetiles.js
@@ -3,21 +3,62 @@
  @constructor
  @description : this class completes a responsive css and is meant to improve the user experience on mobile devices. When the screen is switched from portrait to landscape, the scroll changes form vertical to horizontal and the items are stacked horizontally ("float:left") instead of vertically;
 
- WARNING: the code is not fully abstracted from this specific project. It is therefore not reusable in its current state.
+The code is not fully abstracted from this specific project and uses a lot of global variables. It is therefore not reusable in its current state.
 
  @param  {object} scorll_$ : a JQUERY selection of the container(s) handling the scroll (does not necessarily match the direct parent of the displayed items).
  @param {object} container_$ : a JQUERY selection of the parent containing the items.
  */
 
+/*jslint vars:true, white:true, nomen:true, plusplus:true */
+/*global $, trace, ScreenTools, openItems_array, ArrayTools, project_$, OPEN_WIDTH*/
+
 ResponsiveTiles = function(scorll_$, container_$) {
+	"use strict"; 
 
 	var boxDefaultWidth_num;
 	var boxDefaultHeight_num;
+	
+	//SPECIFIC METHODS FOR THIS PROJECT
+	var _findDefaultHeight = function() {
+
+		if (!boxDefaultHeight_num) {
+
+			var testHeight_num = 10000;
+
+			project_$.each(function(index, element) {
+				var element_$ = $(element);
+
+				var elHeight_num = element_$.outerHeight(true);
+				if (elHeight_num < testHeight_num) {
+					testHeight_num = elHeight_num;
+				}
+			});
+			boxDefaultHeight_num = testHeight_num;
+
+		}
+
+	}; 
+	var _findDefaultWidth = function() {
+
+		if (!boxDefaultWidth_num || boxDefaultWidth_num < 100) {
+			var testWidth_num = 100000;
+			project_$.each(function(index, element) {
+				var element_$ = $(element);
+
+				var elWidth_num = element_$.outerWidth(true);
+
+				if (elWidth_num < testWidth_num) {
+					testWidth_num = elWidth_num;
+				}
+			});
+			boxDefaultWidth_num = testWidth_num;
+
+		}
+	}; 
+	
 
 	this.arrange = function() {
-		var items_$ = container_$.children();
-		var portrait_bool = ScreenTools.isPortrait();
-		var selected_$;
+		var items_$ = container_$.children(), portrait_bool = ScreenTools.isPortrait(),selected_$, scrollPos; 
 
 		if (openItems_array.length > 0) {
 			selected_$ = openItems_array[openItems_array.length - 1];
@@ -25,10 +66,10 @@ ResponsiveTiles = function(scorll_$, container_$) {
 		}
 
 		if (!portrait_bool) {
-			findDefaultWidth();
+			_findDefaultWidth();
 
 			var bodyWidth_num = 0;
-			var scrollPos = -1;
+			scrollPos = -1;
 			items_$.each(function(index, element) {
 
 				var element_$ = $(element);
@@ -48,18 +89,16 @@ ResponsiveTiles = function(scorll_$, container_$) {
 					} else {
 						width_num = boxDefaultWidth_num;
 					}
-				};
+				}
 				bodyWidth_num += width_num;
 
 			});
-
-			scorll_$.css
 			scorll_$.width(bodyWidth_num + 400);
 			var margin_num = 0;
 
-			if (scrollPos != -1) {
-				var cssMargins = parseInt(project_$.css('margin-left')) * 2;
-				var margin_num = (window.innerWidth - OPEN_WIDTH) / 2;
+			if (scrollPos !== -1) {
+				var cssMargins = parseInt(project_$.css('margin-left'),10) * 2;
+				margin_num = (window.innerWidth - OPEN_WIDTH) / 2;
 				scorll_$.animate({
 					scrollLeft : scrollPos - margin_num
 				}, 100);
@@ -67,10 +106,10 @@ ResponsiveTiles = function(scorll_$, container_$) {
 
 		}
 
-		var scrollPos = -1;
+		scrollPos = -1;
 
-		if (ScreenTools.isPortrait() && selected_$ != undefined) {
-			findDefaultHeight();
+		if (ScreenTools.isPortrait() && selected_$ !== undefined) {
+			_findDefaultHeight();
 
 			scrollPos = (Number(selected_$.index() - 1) * boxDefaultHeight_num) + 300;
 
@@ -79,42 +118,7 @@ ResponsiveTiles = function(scorll_$, container_$) {
 			}, 200);
 
 		}
-	}
-	//SPECIFIC METHODS FOR THIS PROJECT
-	findDefaultHeight = function() {
+	}; 
 
-		if (!boxDefaultHeight_num) {
-
-			var testHeight_num = 10000;
-
-			project_$.each(function(index, element) {
-				var element_$ = $(element);
-
-				var elHeight_num = element_$.outerHeight(true);
-				if (elHeight_num < testHeight_num) {
-					testHeight_num = elHeight_num;
-				}
-			});
-			boxDefaultHeight_num = testHeight_num;
-
-		}
-
-	}
-	findDefaultWidth = function() {
-
-		if (!boxDefaultWidth_num || boxDefaultWidth_num < 100) {
-			var testWidth_num = 100000;
-			project_$.each(function(index, element) {
-				var element_$ = $(element);
-
-				var elWidth_num = element_$.outerWidth(true);
-
-				if (elWidth_num < testWidth_num) {
-					testWidth_num = elWidth_num;
-				}
-			});
-			boxDefaultWidth_num = testWidth_num;
-
-		}
-	}
-}
+	
+}; 
--- galleries/slideshow/slideshow_jb.js
+++ galleries/slideshow/slideshow_jb.js
@@ -1,161 +1,144 @@
 /**
-@author : Jean-Baptiste de Borman - jb@noloading.com
+ @author : Jean-Baptiste de Borman - jb@noloading.com
 
-@description : A simple lightbox
+ @description : A simple lightbox
 
-@constructor
-@param  {object} parent_$ : the jquery-html element containing the slideshow.
-@param  {object} imgList_$ : a jquery-selection of <img> tags.
-@param {number} delay-sec : the time before displaying the next image. 
-@param {int} img_height : the height of each image (width is adjusted proportionally).
+ @constructor
+ @param  {object} parent_$ : the jquery-html element containing the slideshow.
+ @param  {object} imgList_$ : a jquery-selection of <img> tags.
+ @param {number} delay-sec : the time before displaying the next image.
+ @param {int} img_height : the height of each image (width is adjusted proportionally).
 
-@function destroy : removes and clean, stops the timer.  
- 
-*/
+ @function destroy : removes and clean, stops the timer.
 
-var Slideshow_jb = function(parent_$, imgList_$, delay_sec, img_height) {
+ */
 
-	var context = this;
-	var currentImg_num;
-	var img_num = imgList_$.length;
+/*jslint vars:true, white:true, nomen:true, plusplus:true */
+/*global $, trace, Timer_jb */
+
+var Slideshow_jb = function(parent_$, imgList_$, delay_sec, img_height) {"use strict";
+
+	var context = this, currentImg_num, n, img_num = imgList_$.length, _show;
 
 	context.container_$ = $("<div>");
-	//context.container_$.width (parent_$.css ('width').split ('px')[0]); 
-	//trace (parent_$.css ('width').split ('px')[0]);
 	context.navigation_$ = $('<div>');
 	context.container_$.append(context.navigation_$);
 	context.container_$.append(imgList_$);
-	//context.container_$.data('slideShow_obj', context);
 	context.height = img_height;
-	
-	
 
 	//PUBLIC METHODS
 
- 	
-
 	this.destroy = function() {
-		//trace ("DESTROY");
 		if (context.timer !== undefined) {
 			context.timer.destroy();
 		}
-		//clearInterval (interval);
 		context.timer = null;
 		context.container_$.remove();
 		context.container_$ = null;
 		context = null;
-		
-	}
+
+	};
 	/// PRIVATE METHODS
 	_show = function(index) {
-		
-		
-		if (context !=null && context != undefined) {
-			
-		context.timer.reset();
-		context.timer.start();
-		index = index % img_num;
 
-		var previousButton_$ = $(context.container_$.find ('.navigationButton')[currentImg_num]);
-		var button_$ = $(context.container_$.find ('.navigationButton')[index]);
-		var img_$ = $($(context.container_$.find ('img'))[index]);
-		var previous_img_$ = $($(context.container_$.find ('img'))[currentImg_num]);
-		var legend_$ = $('<div/>');
-		img_$.css('display', "block");
+		if (context !== null && context !== undefined) {
 
-		previous_img_$.css('opacity', 0);
-		legend_$.addClass('legend');
-		legend_$.text(img_$.attr('alt'));
-		button_$.addClass('selected');
-		previousButton_$.removeClass('selected');
-		currentImg_num = index;
+			context.timer.reset();
+			context.timer.start();
+			index = index % img_num;
 
-		context.container_$.find('.legend').remove();
-		img_$.height(context.height);
-		//img_$.css('left', (context.container_$.width() - img_$.width()) / 2)
+			var previousButton_$ = $(context.container_$.find ('.navigationButton')[currentImg_num]);
+			var button_$ = $(context.container_$.find ('.navigationButton')[index]);
+			var img_$ = $($(context.container_$.find ('img'))[index]);
+			var previous_img_$ = $($(context.container_$.find ('img'))[currentImg_num]);
+			var legend_$ = $('<div/>');
+			img_$.css('display', "block");
 
-		legend_$.css('top', context.height - 20);
+			previous_img_$.css('opacity', 0);
+			legend_$.addClass('legend');
+			legend_$.text(img_$.attr('alt'));
+			button_$.addClass('selected');
+			previousButton_$.removeClass('selected');
+			currentImg_num = index;
 
-		context.container_$.prepend(legend_$);
+			context.container_$.find('.legend').remove();
+			img_$.height(context.height);
 
-		img_$.css('opacity', 1);
+			legend_$.css('top', context.height - 20);
 
-		var margin_pix = context.navigation_$.width();
-		legend_$.css('padding-left', margin_pix).css('padding-right', margin_pix);
-		//legend_$.width(context.container_$.width() - margin_pix * 2);
+			context.container_$.prepend(legend_$);
 
-		legend_$.css('top', context.height + 10).css('opacity', 1);
-	}
-	}
-	_addNav = function() {
+			img_$.css('opacity', 1);
+
+			var margin_pix = context.navigation_$.width();
+			legend_$.css('padding-left', margin_pix).css('padding-right', margin_pix);
+			legend_$.css('top', context.height + 10).css('opacity', 1);
+		}
+	};
+
+	var _addNav = function() {
 		var MARGIN = 30;
-		for (var n = 0; n < imgList_$.length; n++) {
+
+		var showItem = function() {
+			_show($(this).index());
+
+		};
+
+		for ( n = 0; n < imgList_$.length; n++) {
 			var button_$ = $('<div>');
 			button_$.addClass('navigationButton');
 
 			button_$.text(n + 1);
 			context.navigation_$.append(button_$);
 			button_$.css('left', n * MARGIN);
-			button_$.bind('mousedown tap', function() {
-				_show($(this).index());
-			})
+			button_$.bind('mousedown tap', showItem);
 		}
 		context.navigation_$.width((n + 1) * MARGIN);
-	}
-	_init = function() {
-		var interval = window.setInterval (function (){
-		var allImagesAreLoaded_bool = true; 
-		parent_$.prepend(context.container_$);
-		imgList_$.each (function (index, element) {
+	};
+	var _init = function() {
+		var interval = window.setInterval(function() {
+			var allImagesAreLoaded_bool = true;
+			parent_$.prepend(context.container_$);
+			imgList_$.each(function(index, element) {
 
-		
-		if (!element.complete || element.naturalWidth==0) {
-		  	
-		allImagesAreLoaded_bool = false;		
-			}	
-			
-			
-	  }); 
-	  
-		
-		 
+				if (!element.complete || element.naturalWidth === 0) {
+					allImagesAreLoaded_bool = false;
+				}
 
-    if (allImagesAreLoaded_bool) {
-		clearInterval (interval); 
+			});
 
-		if (context != null && context != undefined) {
-	
-			context.container_$.children ('img').each (function (index, element){
+			if (allImagesAreLoaded_bool) {
+				clearInterval(interval);
 
-		
-		
-		}); 
-	
-	
-	
+				if (context !== null && context !== undefined) {
 
-		var hr_$ = $('<hr>');
-		hr_$.addClass('divider');
-		context.container_$.addClass('slideshow');
-		context.container_$.append(hr_$);
-		context.navigation_$.addClass('navigation');
-		context.navigation_$.css('top', context.height).css('left', 0);
-		context.width = parent_$.width();
-		imgList_$.hide();
-		context.container_$.append(this.navigation_$);
-		context.timer = new Timer_jb(null, delay_sec, null);
+					context.container_$.children('img').each(function(index, element) {
 
-		context.timer.onTimerFinish = function() {
-			_show(currentImg_num + 1);
-		}
+					});
 
-		hr_$.css('top', context.height); 
-		_addNav();
-		_show(0);
-		
-}}
-		}, 10)
-		
-		} 
-	_init (); 
-}
+					var hr_$ = $('<hr>');
+					hr_$.addClass('divider');
+					context.container_$.addClass('slideshow');
+					context.container_$.append(hr_$);
+					context.navigation_$.addClass('navigation');
+					context.navigation_$.css('top', context.height).css('left', 0);
+					context.width = parent_$.width();
+					imgList_$.hide();
+					context.container_$.append(this.navigation_$);
+					context.timer = new Timer_jb(null, delay_sec, null);
+
+					context.timer.onTimerFinish = function() {
+						_show(currentImg_num + 1);
+					};
+
+					hr_$.css('top', context.height);
+					_addNav();
+					_show(0);
+
+				}
+			}
+		}, 10);
+
+	};
+	_init();
+};
